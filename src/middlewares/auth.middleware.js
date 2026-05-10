bc861b90a935deee53cc7381fc979745fc637c8e
Miguel300l <miguelledezma400@gmail.com>
2026-05-09 23:52:42 -0500

    Cambiar logica del refreshToken

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,25 +3,38 @@ import Usuario from "../models/Usuario.js";
 
 export const verificarToken = async (req, res, next) => {
     try {
-        let token = req.cookies.token;
+        const token = req.cookies.token;
 
         if (token) {
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            try {
+                const decoded = jwt.verify(
+                    token,
+                    process.env.JWT_SECRET
+                );
 
-            const usuario = await Usuario.findById(decoded.id).populate("rol");
+                const usuario = await Usuario.findById(decoded.id)
+                    .populate("rol");
 
-            if (!usuario) {
-                return res.status(404).json({ message: "Usuario no existe" });
+                if (!usuario) {
+                    return res.status(404).json({
+                        message: "Usuario no existe"
+                    });
+                }
+
+                req.user = usuario;
+                return next();
+
+            } catch (error) {
+                console.log("Access token expirado");
             }
-
-            req.user = usuario;
-            return next();
         }
 
         const refreshToken = req.cookies.refreshToken;
 
         if (!refreshToken) {
-            return res.status(401).json({ message: "No autorizado" });
+            return res.status(401).json({
+                message: "No autorizado"
+            });
         }
 
         const decodedRefresh = jwt.verify(
@@ -29,13 +42,18 @@ export const verificarToken = async (req, res, next) => {
             process.env.JWT_REFRESH_SECRET
         );
 
-        const usuario = await Usuario.findById(decodedRefresh.id).populate("rol");
+        const usuario = await Usuario.findById(decodedRefresh.id)
+            .populate("rol");
 
         if (!usuario) {
-            return res.status(404).json({ message: "Usuario no existe" });
+            return res.status(404).json({
+                message: "Usuario no existe"
+            });
         }
 
-        const newAccessToken = createAccessToken({ id: usuario._id });
+        const newAccessToken = createAccessToken({
+            id: usuario._id
+        });
 
         res.cookie("token", newAccessToken, {
             httpOnly: true,
@@ -48,7 +66,9 @@ export const verificarToken = async (req, res, next) => {
         next();
 
     } catch (error) {
-        return res.status(401).json({ message: "Token inválido o expirado" });
+        return res.status(401).json({
+            message: "Refresh token inválido o expirado"
+        });
     }
 };
 
