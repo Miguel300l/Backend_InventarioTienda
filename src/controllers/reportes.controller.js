ac913d53c7a0c4939c97915e6c5b7c48a57285f4
Miguel300l <miguelledezma400@gmail.com>
2026-05-12 00:15:45 -0500

    Mejorar endpoint de reportes

--- src/controllers/reportes.controller.js
+++ src/controllers/reportes.controller.js
@@ -25,13 +25,45 @@ export const proveedorMasBarato = async (req, res) => {
                     },
                 },
             },
+
+            {
+                $lookup: {
+                    from: "proveedors",
+                    localField: "_id",
+                    foreignField: "_id",
+                    as: "proveedor",
+                },
+            },
+
+            {
+                $unwind: "$proveedor",
+            },
+
             {
                 $sort: {
                     precioMasBarato: 1,
                 },
             },
             {
-                $limit: 1,
+                $project: {
+                    _id: 0,
+
+                    proveedorId: "$proveedor._id",
+
+                    nombreProveedor: "$proveedor.nombre",
+
+                    correoProveedor: "$proveedor.correo",
+
+                    telefonoProveedor: "$proveedor.telefono",
+
+                    precioMasBarato: 1,
+
+                    promedioPrecio: {
+                        $round: ["$promedioPrecio", 2],
+                    },
+
+                    totalCompras: 1,
+                },
             },
         ]);
 
@@ -41,7 +73,7 @@ export const proveedorMasBarato = async (req, res) => {
             });
         }
 
-        return res.json(resultado[0]);
+        return res.json(resultado);
 
     } catch (error) {
         return res.status(500).json({
